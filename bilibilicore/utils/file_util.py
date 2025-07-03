15efcbedf125993f3efb6f3b272cede6d95b3781
lijunjie2232 <2533584225@qq.com>
2025-07-03 21:08:33 +0900

    file util

---
+++ bilibilicore/utils/file_util.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+import os
+
+
+def check_and_mkdir(path):
+    path = Path(path)
+    if not path.is_dir():
+        path.mkdir(
+            parents=True,
+            exist_ok=True,
+        )
+    return True
+
+
+def get_app_data_dir(app_name: str, makedir=False) -> Path:
+    # Determine the OS-specific app data directory
+    app_data_path = None
+    if os.name == "nt":  # Windows
+        app_data_path = Path(os.getenv("APPDATA"))
+    else:  # macOS/Linux
+        app_data_path = Path.home() / ".local" / "share"
+
+    # Create the application-specific directory
+    app_dir = app_data_path / app_name
+
+    # Ensure the directory exists
+    if makedir:
+        app_dir.mkdir(
+            parents=True,
+            exist_ok=True,
+        )
+
+    return app_dir
